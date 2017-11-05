6211e7a4f165e00b5d502f949f147c08ffcd648e
Sadi Nasib <sadinasib@hotmail.com>
2017-11-05 23:24:02 +0100

    Added UI to add and edit words

--- app/src/main/java/com/sadinasib/notebook/EditorActivity.java
+++ app/src/main/java/com/sadinasib/notebook/EditorActivity.java
@@ -1,13 +1,209 @@
 package com.sadinasib.notebook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.sadinasib.notebook.data.NotebookContract.NotebookEntry;
 
 public class EditorActivity extends AppCompatActivity {
+    private static final String TAG = EditorActivity.class.getSimpleName();
+    private EditText mWordEdit;
+    private EditText mTransEdit;
+    private Uri mWordUri;
+    private boolean mEditTextHasChanged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mWordEdit = findViewById(R.id.editor_edit_word);
+        mTransEdit = findViewById(R.id.editor_edit_trans);
+
+        Intent intent = getIntent();
+        mWordUri = intent.getData();
+
+        if (mWordUri == null) {
+            setTitle(getString(R.string.editor_activity_title_add));
+            invalidateOptionsMenu();
+        } else {
+            setTitle(getString(R.string.editor_activity_title_edit));
+            updateUI();
+        }
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mEditTextHasChanged = true;
+            }
+        };
+        mWordEdit.addTextChangedListener(textWatcher);
+        mTransEdit.addTextChangedListener(textWatcher);
+    }
+
+    private void updateUI() {
+        Log.i(TAG, "updateUI");
+        String[] projection = {
+                NotebookEntry._ID,
+                NotebookEntry.COLUMN_WORD,
+                NotebookEntry.COLUMN_TRANSLATION};
+
+        Cursor cursor = getContentResolver().query(mWordUri, projection, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            mWordEdit.setText(cursor.getString(cursor.getColumnIndexOrThrow(NotebookEntry.COLUMN_WORD)));
+            mTransEdit.setText(cursor.getString(cursor.getColumnIndexOrThrow(NotebookEntry.COLUMN_TRANSLATION)));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mWordUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_editor_delete);
+            menuItem.setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_editor_save:
+                saveWord();
+                finish();
+                return true;
+            case R.id.action_editor_delete:
+                showDeleteConfirmationDialog();
+                return true;
+            case android.R.id.home:
+                if (mEditTextHasChanged) {
+                    showUnsavedChangesDialog();
+                } else {
+                    mEditTextHasChanged = false;
+                    NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveWord() {
+        Log.i(TAG, "saveWord");
+        String word = mWordEdit.getText().toString().trim();
+        String trans = mTransEdit.getText().toString().trim();
+
+        if (TextUtils.isEmpty(word)
+                && TextUtils.isEmpty(trans)) {
+            Toast.makeText(this, R.string.editor_activity_no_data, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        if (TextUtils.isEmpty(word)) {
+            Toast.makeText(this, R.string.editor_activity_need_word, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        if (TextUtils.isEmpty(trans)) {
+            Toast.makeText(this, R.string.editor_activity_need_trans, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(NotebookEntry.COLUMN_WORD, word);
+        values.put(NotebookEntry.COLUMN_TRANSLATION, trans);
+
+        if (mWordUri == null) {
+            Uri newUri = getContentResolver().insert(NotebookEntry.CONTENT_URI, values);
+            if (newUri == null) {
+                Log.e(TAG, "saveWord failed for uri " + mWordUri.toString());
+                Toast.makeText(this, R.string.editor_activity_save_failed, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.editor_activity_save_succ, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            int rowId = getContentResolver().update(mWordUri, values, null, null);
+            if (rowId == 0) {
+                Log.e(TAG, "saveWord failed for uri " + mWordUri.toString());
+                Toast.makeText(this, R.string.editor_activity_update_failed, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.editor_activity_update_succ, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void deleteWord() {
+        int rowId = getContentResolver().delete(mWordUri, null, null);
+        if (rowId == 0) {
+            Log.e(TAG, "deleteWord failed for uri " + mWordUri.toString());
+            Toast.makeText(this, R.string.editor_activity_delete_failed, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, R.string.editor_activity_delete_succ, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showUnsavedChangesDialog() {
+        Log.i(TAG, "showUnsavedChangesDialog");
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.unsaved_changes_dialog_title)
+                .setMessage(R.string.unsaved_changes_dialog_msg)
+                .setPositiveButton(R.string.unsaved_changes_dialog_keep_editing, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setNegativeButton(R.string.unsaved_changes_dialog_discard, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                    }
+                })
+                .show();
+    }
+
+    private void showDeleteConfirmationDialog() {
+        Log.i(TAG, "showDeleteConfirmationDialog");
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.delete_confirm_dialog_title)
+                .setMessage(R.string.delete_confirm_dialog_msg)
+                .setPositiveButton(R.string.delete_confirm_dialog_delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteWord();
+                        finish();
+                    }
+                })
+                .setNegativeButton(R.string.delete_confirm_dialog_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .show();
     }
 }
